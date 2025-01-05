5199b9b8939075b9d5bbd384e83bac8be3319ccb
timofejkazakov <t.kazakov@agora.ru>
2025-01-05 18:07:56 +0300

    Логи

--- internal/server/services/file_service.go
+++ internal/server/services/file_service.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"time"
 
+	log "github.com/sirupsen/logrus"
+
 	"github.com/Axel791/metricsalert/internal/server/repositories"
 )
 
@@ -36,15 +38,19 @@ func (s *FileStorageService) Save() error {
 // StartAutoSave запускает периодическое сохранение
 func (s *FileStorageService) StartAutoSave(ctx context.Context) {
 	if s.storeInterval <= 0 {
+		log.Infof("Auto-save disabled (storeInterval=%v)", s.storeInterval)
 		return
 	}
 
-	ticker := time.NewTicker(s.storeInterval)
+	ticker := time.NewTicker(s.storeInterval * time.Second)
+	log.Infof("Starting auto-save every %s", s.storeInterval)
+
 	go func() {
 		defer ticker.Stop()
 		for {
 			select {
 			case <-ctx.Done():
+				log.Info("Auto-save context done, stopping...")
 				return
 			case <-ticker.C:
 				_ = s.Save()
