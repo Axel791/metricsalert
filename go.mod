b78098d1bdffb7609afa3ff815cd10c73d35bc44
timofejkazakov <t.kazakov@agora.ru>
2025-01-15 22:35:09 +0300

    Добавил retry операции

--- go.mod
+++ go.mod
@@ -6,8 +6,11 @@ require (
 	github.com/Masterminds/squirrel v1.5.4
 	github.com/go-chi/chi/v5 v5.1.0
 	github.com/gojek/heimdall/v7 v7.0.3
+	github.com/jackc/pgconn v1.14.3
+	github.com/jackc/pgerrcode v0.0.0-20240316143900-6e2875d9b438
 	github.com/jmoiron/sqlx v1.4.0
 	github.com/lib/pq v1.10.9
+	github.com/pkg/errors v0.9.1
 	github.com/pressly/goose/v3 v3.24.1
 	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/viper v1.19.0
@@ -20,13 +23,17 @@ require (
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/gojek/valkyrie v0.0.0-20180215180059-6aee720afcdf // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
 	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mfridman/interpolate v0.0.2 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
@@ -38,6 +45,7 @@ require (
 	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
 	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
 	golang.org/x/sync v0.10.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
--- internal/server/repositories/metrics.go
+++ internal/server/repositories/metrics.go
@@ -27,146 +27,171 @@ func NewMetricRepository(db *sqlx.DB) *MetricsRepositoryHandler {
 
 // UpdateGauge - обновление Gauge.
 func (r *MetricsRepositoryHandler) UpdateGauge(ctx context.Context, name string, value float64) (domain.Metrics, error) {
-	metric := domain.Metrics{
-		ID:    name,
-		MType: domain.Gauge,
-		Value: null.FloatFrom(value),
-		Delta: null.Int{},
-	}
+	var result domain.Metrics
 
-	query, args, err := cursor.
-		Insert("metrics").
-		Columns("id", "metric_type", "value", "delta").
-		Values(metric.ID, metric.MType, metric.Value, nil).
-		Suffix(`
-			ON CONFLICT (id) 
-			DO UPDATE SET
-				metric_type = EXCLUDED.metric_type,
-				value       = EXCLUDED.value,
-				delta       = NULL
-		`).
-		ToSql()
+	err := RetryOperation(func() error {
+		metric := domain.Metrics{
+			ID:    name,
+			MType: domain.Gauge,
+			Value: null.FloatFrom(value),
+			Delta: null.Int{},
+		}
 
-	if err != nil {
-		return domain.Metrics{}, fmt.Errorf("building upsert gauge query: %w", err)
-	}
+		query, args, err := cursor.
+			Insert("metrics").
+			Columns("id", "metric_type", "value", "delta").
+			Values(metric.ID, metric.MType, metric.Value, nil).
+			Suffix(`
+				ON CONFLICT (id) 
+				DO UPDATE SET
+					metric_type = EXCLUDED.metric_type,
+					value       = EXCLUDED.value,
+					delta       = NULL
+			`).
+			ToSql()
 
-	_, execErr := r.db.ExecContext(ctx, query, args...)
-	if execErr != nil {
-		return domain.Metrics{}, fmt.Errorf("exec upsert gauge query: %w", execErr)
-	}
+		if err != nil {
+			return fmt.Errorf("building upsert gauge query: %w", err)
+		}
 
-	return metric, nil
+		_, execErr := r.db.ExecContext(ctx, query, args...)
+		if execErr != nil {
+			return fmt.Errorf("exec upsert gauge query: %w", execErr)
+		}
+
+		result = metric
+		return nil
+	})
+
+	return result, err
 }
 
 // UpdateCounter - обновление counter
 func (r *MetricsRepositoryHandler) UpdateCounter(ctx context.Context, name string, value int64) (domain.Metrics, error) {
-	metric := domain.Metrics{
-		ID:    name,
-		MType: domain.Counter,
-		Delta: null.IntFrom(value),
-		Value: null.Float{},
-	}
+	var result domain.Metrics
 
-	query, args, err := cursor.
-		Insert("metrics").
-		Columns("id", "metric_type", "delta", "value").
-		Values(metric.ID, metric.MType, metric.Delta, nil).
-		Suffix(`
-			ON CONFLICT (id) 
-			DO UPDATE SET
-				metric_type = EXCLUDED.metric_type,
-				delta       = metrics.delta + EXCLUDED.delta,
-				value       = NULL
-		`).
-		ToSql()
-	if err != nil {
-		return domain.Metrics{}, fmt.Errorf("building upsert counter query: %w", err)
-	}
+	err := RetryOperation(func() error {
+		metric := domain.Metrics{
+			ID:    name,
+			MType: domain.Counter,
+			Delta: null.IntFrom(value),
+			Value: null.Float{},
+		}
 
-	_, execErr := r.db.ExecContext(ctx, query, args...)
-	if execErr != nil {
-		return domain.Metrics{}, fmt.Errorf("exec upsert counter query: %w", execErr)
-	}
+		query, args, err := cursor.
+			Insert("metrics").
+			Columns("id", "metric_type", "delta", "value").
+			Values(metric.ID, metric.MType, metric.Delta, nil).
+			Suffix(`
+				ON CONFLICT (id) 
+				DO UPDATE SET
+					metric_type = EXCLUDED.metric_type,
+					delta       = metrics.delta + EXCLUDED.delta,
+					value       = NULL
+			`).
+			ToSql()
+		if err != nil {
+			return fmt.Errorf("building upsert counter query: %w", err)
+		}
 
-	return metric, nil
+		_, execErr := r.db.ExecContext(ctx, query, args...)
+		if execErr != nil {
+			return fmt.Errorf("exec upsert counter query: %w", execErr)
+		}
+
+		result = metric
+		return nil
+	})
+
+	return result, err
 }
 
 // GetMetric - получение метрики по ее ID.
 func (r *MetricsRepositoryHandler) GetMetric(ctx context.Context, metric domain.Metrics) (domain.Metrics, error) {
-	query, args, err := cursor.
-		Select("id", "metric_type", "value", "delta").
-		From("metrics").
-		Where(sq.Eq{"id": metric.ID}).
-		Limit(1).
-		ToSql()
-	if err != nil {
-		return domain.Metrics{}, fmt.Errorf("build get metric query: %w", err)
-	}
-
 	var result domain.Metrics
-	err = r.db.GetContext(ctx, &result, query, args...)
-	if err != nil {
-		return domain.Metrics{}, fmt.Errorf("get metric from db: %w", err)
-	}
 
-	return result, nil
+	err := RetryOperation(func() error {
+		query, args, err := cursor.
+			Select("id", "metric_type", "value", "delta").
+			From("metrics").
+			Where(sq.Eq{"id": metric.ID}).
+			Limit(1).
+			ToSql()
+		if err != nil {
+			return fmt.Errorf("build get metric query: %w", err)
+		}
+
+		err = r.db.GetContext(ctx, &result, query, args...)
+		if err != nil {
+			return fmt.Errorf("get metric from db: %w", err)
+		}
+
+		return nil
+	})
+
+	return result, err
 }
 
 // GetAllMetrics - Получение всех метрик
 func (r *MetricsRepositoryHandler) GetAllMetrics(ctx context.Context) (map[string]domain.Metrics, error) {
-	query, args, err := cursor.
-		Select("id", "metric_type", "value", "delta").
-		From("metrics").
-		ToSql()
-	if err != nil {
-		return nil, fmt.Errorf("build get all metrics query: %w", err)
-	}
-
-	rows, err := r.db.QueryxContext(ctx, query, args...)
-	if err != nil {
-		return nil, fmt.Errorf("query all metrics: %w", err)
-	}
-	defer rows.Close()
-
 	metricsMap := make(map[string]domain.Metrics)
 
-	for rows.Next() {
-		var m domain.Metrics
-		if scanErr := rows.StructScan(&m); scanErr != nil {
-			return nil, fmt.Errorf("scan metric row: %w", scanErr)
+	err := RetryOperation(func() error {
+		query, args, err := cursor.
+			Select("id", "metric_type", "value", "delta").
+			From("metrics").
+			ToSql()
+		if err != nil {
+			return fmt.Errorf("build get all metrics query: %w", err)
 		}
-		metricsMap[m.ID] = m
-	}
 
-	return metricsMap, nil
+		rows, err := r.db.QueryxContext(ctx, query, args...)
+		if err != nil {
+			return fmt.Errorf("query all metrics: %w", err)
+		}
+		defer rows.Close()
+
+		for rows.Next() {
+			var m domain.Metrics
+			if scanErr := rows.StructScan(&m); scanErr != nil {
+				return fmt.Errorf("scan metric row: %w", scanErr)
+			}
+			metricsMap[m.ID] = m
+		}
+
+		return nil
+	})
+
+	return metricsMap, err
 }
 
 // BatchUpdateMetrics - обновление метрик батчами
 func (r *MetricsRepositoryHandler) BatchUpdateMetrics(ctx context.Context, metrics []domain.Metrics) error {
-	insertBuilder := cursor.Insert("metrics").
-		Columns("id", "metric_type", "value", "delta")
+	return RetryOperation(func() error {
+		insertBuilder := cursor.Insert("metrics").
+			Columns("id", "metric_type", "value", "delta")
 
-	for _, metric := range metrics {
-		insertBuilder = insertBuilder.Values(metric.ID, metric.MType, metric.Value, metric.Delta)
-	}
+		for _, metric := range metrics {
+			insertBuilder = insertBuilder.Values(metric.ID, metric.MType, metric.Value, metric.Delta)
+		}
 
-	sql, args, err := insertBuilder.ToSql()
-	if err != nil {
-		return fmt.Errorf("cannot build insert query: %w", err)
-	}
+		sql, args, err := insertBuilder.ToSql()
+		if err != nil {
+			return fmt.Errorf("cannot build insert query: %w", err)
+		}
 
-	sql += `
-        ON CONFLICT (id)
-        DO UPDATE SET
-            metric_type = EXCLUDED.metric_type,
-            value       = EXCLUDED.value,
-            delta       = EXCLUDED.delta
-    `
+		sql += `
+			ON CONFLICT (id)
+			DO UPDATE SET
+				metric_type = EXCLUDED.metric_type,
+				value       = EXCLUDED.value,
+				delta       = EXCLUDED.delta
+		`
 
-	if _, err := r.db.ExecContext(ctx, sql, args...); err != nil {
-		return fmt.Errorf("cannot exec batch upsert: %w", err)
-	}
+		if _, err := r.db.ExecContext(ctx, sql, args...); err != nil {
+			return fmt.Errorf("cannot exec batch upsert: %w", err)
+		}
 
-	return nil
+		return nil
+	})
 }
