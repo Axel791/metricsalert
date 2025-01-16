ad13abbe9f13bf2cdfb650f09e3bb2b24884a945
timofejkazakov <t.kazakov@agora.ru>
2025-01-16 23:43:20 +0300

    Доп логи

--- internal/server/config/flag.go
+++ internal/server/config/flag.go
@@ -10,14 +10,18 @@ func ParseFlags(cfg *Config) (string, string, int64, string, bool) {
 
 	databaseDSN := flag.String("d", cfg.DatabaseDSN, "database DSN")
 
-	log.Infof("databasedsn: %s", *databaseDSN)
-
 	storeIntervalFlag := flag.Int64(
 		"i", cfg.StoreInterval, "interval in seconds for storing metrics (0 means sync)",
 	)
 	filePathFlag := flag.String("f", cfg.FileStoragePath, "path to file for storing metrics")
 	restoreFlag := flag.Bool("r", cfg.Restore, "restore metrics from file on start (true/false)")
-
 	flag.Parse()
+
+	log.Infof("addr %s", *addr)
+	log.Infof("databasedsn: %s", *databaseDSN)
+	log.Infof("store interval: %d", *storeIntervalFlag)
+	log.Infof("file path: %s", *filePathFlag)
+	log.Infof("restore interval: %v", *restoreFlag)
+
 	return *addr, *databaseDSN, *storeIntervalFlag, *filePathFlag, *restoreFlag
 }
