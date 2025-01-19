289e262125d92e2de43e938a088f0c5c67dfdaab
timofejkazakov <t.kazakov@agora.ru>
2025-01-19 16:11:57 +0300

    Правка конфига

--- internal/server/config/config.go
+++ internal/server/config/config.go
@@ -3,18 +3,17 @@ package config
 import (
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
-	"strings"
 )
 
 // Config структура для хранения конфигурации
 type Config struct {
-	Address         string `mapstructure:"address"`
-	StoreInterval   int64  `mapstructure:"store_interval"`
-	FileStoragePath string `mapstructure:"file_storage_path"`
-	Restore         bool   `mapstructure:"restore"`
-	UseFileStorage  bool   `mapstructure:"use_file_storage"`
-	DatabaseDSN     string `mapstructure:"database_dsn"`
-	MigrationsPath  string `mapstructure:"migrations_path"`
+	Address         string `mapstructure:"ADDRESS"`
+	StoreInterval   int64  `mapstructure:"STORE_INTERVAL"`
+	FileStoragePath string `mapstructure:"FILE_STORAGE_PATH"`
+	Restore         bool   `mapstructure:"RESTORE"`
+	UseFileStorage  bool   `mapstructure:"USE_FILE_STORAGE"`
+	DatabaseDSN     string `mapstructure:"DATABASE_DSN"`
+	MigrationsPath  string `mapstructure:"MIGRATIONS_PATH"`
 }
 
 // ServerLoadConfig загружает конфигурацию из .env, переменных окружения и задает значения по умолчанию
@@ -32,7 +31,13 @@ func ServerLoadConfig() (*Config, error) {
 
 	viper.AutomaticEnv()
 
-	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_", "-", "_"))
+	_ = viper.BindEnv("ADDRESS", "ADDRESS")
+	_ = viper.BindEnv("STORE_INTERVAL", "STORE_INTERVAL")
+	_ = viper.BindEnv("FILE_STORAGE_PATH", "FILE_STORAGE_PATH")
+	_ = viper.BindEnv("RESTORE", "RESTORE")
+	_ = viper.BindEnv("USE_FILE_STORAGE", "USE_FILE_STORAGE")
+	_ = viper.BindEnv("DATABASE_DSN", "DATABASE_DSN")
+	_ = viper.BindEnv("MIGRATIONS_PATH", "MIGRATIONS_PATH")
 
 	if err := viper.ReadInConfig(); err != nil {
 		log.Infof("filed find file config set defoult value: %v", err)
