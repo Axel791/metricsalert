b7102bd304c24caea19e7219442718388c89b88d
timofejkazakov <t.kazakov@agora.ru>
2025-01-19 17:18:07 +0300

    Изменил миграцию

--- migrations/20250112193902_init_schema.sql
+++ migrations/20250112193902_init_schema.sql
@@ -3,8 +3,8 @@
 CREATE TABLE metrics (
     id TEXT PRIMARY KEY,
     metric_type VARCHAR(10) NOT NULL CHECK (metric_type IN ('gauge', 'counter')),
-    value FLOAT,
-    delta INT
+    value DOUBLE PRECISION,
+    delta BIGINT
 );
 -- +goose StatementEnd
 
