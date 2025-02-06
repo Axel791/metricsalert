083b3eb8c152a53951cd27e3d9ade00f1ed64097
timofejkazakov <t.kazakov@agora.ru>
2025-02-06 23:46:08 +0300

    Доработка

--- internal/server/handlers/update_metric.go
+++ internal/server/handlers/update_metric.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 
@@ -33,14 +34,21 @@ func NewUpdateMetricHandler(
 func (h *UpdateMetricHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	token := r.Header.Get("HashSHA256")
 
-	h.logger.Infof("token: %s", token)
+	h.logger.Infof("token update: %s", token)
 
 	validBody, err := h.validateBody(r)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	if err = h.authService.Validate(token, validBody); err != nil {
+	normalizedBody, err := h.normalizeBody(validBody)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err = h.authService.Validate(token, normalizedBody); err != nil {
 		h.logger.Infof("error: %v", err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
@@ -81,4 +89,19 @@ func (h *UpdateMetricHandler) validateBody(r *http.Request) ([]byte, error) {
 		return nil, nil
 	}
 	return body, nil
+}
+
+func (h *UpdateMetricHandler) normalizeBody(body []byte) ([]byte, error) {
+	var metricsList []api.Metrics
+	if err := json.Unmarshal(body, &metricsList); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal body: %w", err)
+	}
+	normalizedBody, err := json.Marshal(metricsList)
+
+	h.logger.Infof("normalized body update metric: %s", string(normalizedBody))
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal normalized body: %w", err)
+	}
+	return normalizedBody, nil
 }
